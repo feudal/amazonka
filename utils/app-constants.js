567befb2beb9597112e60afa556c9f7c0c585a1c
Dmitri Calmîș <calmis87@gmail.com>
2022-08-24 20:58:54 +0300

    add constant payment_steps

--- utils/app-constants.js
+++ utils/app-constants.js
@@ -3,4 +3,14 @@ export const ACTIONS = {
   CART_REMOVE_ITEM: "CART_REMOVE_ITEM",
   CART_RESET: "CART_RESET",
   SAVE_SHIPPING_ADDRESS: "SAVE_SHIPPING_ADDRESS",
+  SAVE_PAYMENT_METHOD: "SAVE_PAYMENT_METHOD",
 };
+
+export const PAYMENT_STEPS = [
+  "User Login",
+  "Shipping Address",
+  "Payment Method",
+  "Place Order",
+];
+
+export const PAYMENT_METHODS = ["PayPall", "Stripe", "CashOnDelivery"];
